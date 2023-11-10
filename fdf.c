5cf49f5bf7c3de91b843ade140b4a107883fd146
charles1812 <109655164+charles1812@users.noreply.github.com>
2023-11-10 18:35:50 +0100

    Add files via upload

--- fdf.c
+++ fdf.c
@@ -1,11 +1,26 @@
 #include "fdf.h"
 
+void    ft_picel_out(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
+    dst
+}
+
+
 int     main(void)
 {
     void    *mlx;
     void    *mlx_win;
+    t_data  img;
 
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    mlx_loop(mlx);
+   /* img.img = mlx_new_image(mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
+    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    */
+   mlx_loop(mlx);
 }
--- fdf.h
+++ fdf.h
@@ -4,4 +4,13 @@
 #include "libft/libft.h"
 #include "minilibx-linux/mlx.h"
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}t_data;
+
+
 #endif
